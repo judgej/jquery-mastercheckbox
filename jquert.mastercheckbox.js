266bd08886d19df0714532c9614d85c0c8c647a5
Jason Judge <jason.judge@consil.co.uk>
2013-11-02 11:07:47 +0000

    Further dev.

--- jquert.mastercheckbox.js
+++ jquert.mastercheckbox.js
@@ -2,19 +2,36 @@
  * Need two selection entry points:
  *	* The group selector. Whatever collects the related checkboxes together.
  *	* The master checkbox. A selector that identifies the master.
+ * Visually, the checkboxes would look something like this:
+ * 
+ * [x] Select/deselect All
+ * [x] Option 1
+ * [x] Option 2 etc.
+ * 
+ * Option 1, 2 etc. would all follow the state of the master checkbox at the top,
+ * if the master checkbox is checked or unchecked. The master checkbox would then
+ * also follow the state of the other options, if any change of state results in
+ * all or not-all being checked.
+ *
+ * TODO:
+ *	* Make this into a jQuery plugin.
+ *	* Accept parameters for the selectors.
  */
 
 jQuery(document).ready(function($) {
+	var group_selector = '#set-1 input:checkbox';
+	var master_selector = '#set-1 .master input:checkbox';
+
 	/* If the master checkbox is toggled, then set all other checkboxes in the group the same. */
-	$('#set-1 .master input:checkbox').click(function() {
+	$(master_selector).click(function() {
 		var status = this.checked;
 		var master = this;
-		$('#set-1 input:checkbox').each(function() {
+		$(group_selector).each(function() {
 			if (this !== master) $(this).prop('checked', status);
 		});
 	});
 
-	$('#set-1 input:checkbox').not('.master').click(function() {
+	$(group_selector).click(function() {
 		/* 
 			If all checkboxes are unchecked, then make sure the master is checked,
 			otherwise it should be unchecked.
@@ -22,14 +39,16 @@ jQuery(document).ready(function($) {
 		// True if all options selected
 		var master_status = true;
 
-		// Find the master checkbox.
-		var master = $('#set-1 .master input:checkbox')[0];
+		// Get the master checkbox.
+		var master = $(master_selector)[0];
 
-		$('#set-1 input:checkbox').each(function() {
+		$(group_selector).each(function() {
 			if (this !== master && this.checked == false) {
 				master_status = false;
 			}
 		});
+
+		// Change the master checked status if it needs changing.
 		if (master_status !== master.checked) {
 			$(master).prop('checked', master_status);
 		}
